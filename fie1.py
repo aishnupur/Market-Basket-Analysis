d24d2885dff6edc6410ca94edc0a55f4168639fd
Tapan Golakiya <golakiyatapan@gmail.com>
2019-04-04 17:48:33 -0400

    commit-2
    assosiation rules produced

--- fie1.py
+++ fie1.py
@@ -1,26 +1,28 @@
 import pandas as pd
 import matplotlib as pl
 import numpy as np
-
+from apyori import apriori
 dataset1 = pd.read_csv('train.csv')
 
 print('Total products in dataset:',dataset1['Upc'].nunique())
 print('Total transactions in dataset:',dataset1['VisitNumber'].nunique())
 print('Total Departments in walmart:',dataset1['DepartmentDescription'].nunique())
 
-
 col = ['VisitNumber','Upc']
 dataset2 = dataset1[col]
 
 dataset3 = dataset2.groupby('VisitNumber')['Upc'].apply(list)
 
-#deletes rows which have description as 'Discount'
-for index, row in dataset1.iterrows():
-    if row['Description'] == 'Discount':
-        dataset1.drop(index,inplace =True)
-    elif row.IsNull('Description'):
-        print(row['InvoiceNo'])
+frequent_itemsets = apriori(dataset3, min_support=0.0006, min_confidence=0.5, min_lift=3, min_length=2)
+association_results = list(frequent_itemsets)
+print(association_results[0])  
 
-for index, row in dataset1.iterrows():
-    if row['Description'] == DBNull.Value:
-        print(row['InvoiceNo'])
+for item in association_results:
+    pair = item[0] 
+    items = [x for x in pair]
+    print("Rule: ",items[0], " -> " ,items[1])
+
+    print("Support: " + str(item[1]))
+    print("Confidence: " + str(item[2][0][2]))
+    print("Lift: " + str(item[2][0][3]))
+    print("=====================================")
--- file1.R
+++ file1.R
@@ -5,15 +5,20 @@ dataset1 <- read.csv('train.csv')
 dataset2 <- data.frame(dataset1$VisitNumber, dataset1$Upc)
 names(dataset2) <- c("transaction", "product")
 
+library(plyr)
+ordered_data <- ddply(dataset2,c("transaction"),
+                      function(df1)paste(df1$product,collapse = ","))
+write.csv(ordered_data,"ordered_data.csv",quote = FALSE,row.names = TRUE)
+
+txn = read.transactions(file = "ordered_data.csv",rm.duplicates = TRUE,format = "basket",sep = ",",cols=1)
+basket_rules <- apriori(txn,parameter = list(sup = 0.005,conf = 0.5,target = "rules"))
+inspect(basket_rules)
+inspect(sort(basket_rules,by='lift')[1:100])
+summary(basket_rules)
 
 dataset3 <- aggregate(dataset2$product ~ dataset2$transaction, dataset2, c)
 names(dataset3) <- c("transaction", "products")
 
-#library(data.table)
-#dataset3 <- as.data.table(dataset2)[,list(list(dataset2$product)), by = dataset2$transaction]
-
-data("Groceries")
-head(dataset3, 2)
 
 rules <- apriori(dataset3, parameter = list(supp = 0.2, conf = 0.2)) # Min Support as 0.001, confidence as 0.8.
 rules_conf <- sort (rules, by="confidence", decreasing=TRUE)
